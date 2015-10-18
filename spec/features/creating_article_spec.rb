9cee50995d9364bc02cf96db1b4bc7dcdbc0f359
Mohamed Abdelrahman <mohamed@interactegypt.me>
2015-10-18 14:30:37 +0200

    first feature rspec

---
+++ spec/features/creating_article_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature "Creating Articles" do
+  scenario "A user create a new article" do
+
+    visit "/"
+
+    click_link "New Article"
+
+    fill_in "Title",with: "Creating first article"
+    fill_in "Body", with: "Lorem Ipsum"
+
+    click_button "Create Article"
+
+    expect(page).to have_content("Article has been created")
+    expect(page.current_path).to eql(articles_path)
+
+  end
+end
